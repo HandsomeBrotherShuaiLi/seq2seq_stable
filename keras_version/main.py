abd5fd9e107029963b06d0a6dbd1484d4325af92
李帅@NLP <tongjicslishuai@gmail.com>
2019-08-31 11:34:02 +0800

    add LSTM

--- keras_version/main.py
+++ keras_version/main.py
@@ -1,6 +1,10 @@
 import numpy as np
 from collections import defaultdict
-from keras.layers import Input,LSTM,GRU,Bidirectional,Lambda
+from keras.layers import Input,LSTM,GRU,Bidirectional,Lambda,Embedding
+from keras.preprocessing.text import Tokenizer
+from keras.preprocessing.sequence import pad_sequences
+import keras.backend as K
+
 import json
 class Data(object):
     def __init__(self,train_data_path='../Data/train_3.txt',
@@ -49,6 +53,7 @@ class Data(object):
                     temp_index=index[id:]+index[:(id+self.batch_size)%len(data)]
                     samples=data[temp_index]
                 utt_all = []
+                utt_len=[]
                 for sample in samples:
                     temp=sample.split('\t')
                     r=temp[1].split(' ')
@@ -61,38 +66,65 @@ class Data(object):
                             utt_id.append([self.dict[j] for j in utt_lines[i]])
                         except:
                             utt_id.append([self.dict['<pad>']])
+                        utt_len.append(len(utt_id[-1]))
                     utt_all.append(utt_id)
 
                 for i in range(self.max_sen):
-                    encoder_input.append(utt_all[j][i] for j in range(self.batch_size))
+                    line_sample=[utt_all[j][i] for j in range(self.batch_size)]
+                    line_sample=pad_sequences(line_sample,maxlen=max(utt_len))
+                    encoder_input.append(line_sample)
                 id=(id+self.batch_size)%(len(data))
                 """
                 encoder是固定max_sen行，不定长句子的list
                 """
-                print(len(encoder_input),len(decoder_input))
+                print(len(encoder_input),len(encoder_input[0]),print(len(encoder_input[0][0]),len(encoder_input[0][1])))
                 yield encoder_input,decoder_input
 
         else:
             pass
 
 class seq2seq(object):
-    def __init__(self,hidden,max_len=250,max_sen=11):
-        self.max_vocab_len=max_len
+    def __init__(self,hidden,max_vocab_len,max_sentence_len=250,max_sen=11,batch_size=12):
+        self.max_vocab_len=max_vocab_len
+        self.max_sentence_len=max_sentence_len
         self.max_sen=max_sen
         self.hidden=hidden
+        self.batch_size=batch_size
+    def encoder_input(self,x):
+        embedding_layer=Embedding(self.max_vocab_len,self.hidden,input_length=None,batch_size=self.batch_size)
+        lstm_layer=Bidirectional(LSTM(self.hidden,return_sequences=True,return_state=True),merge_mode='sum')
+        shape=x.get_shape()
+        for line in shape[0]:
+            line_tensor=x[line,:,:]
+            emb=embedding_layer(line_tensor)
+            lstm,sequences,states=lstm_layer(emb)
+
+
+
+
+
     def build_network(self):
-        layer=Lambda(function=)
+        encoder_input=Lambda()
         encoder_input=Input(shape=(None,self.max_vocab_len),name='encoder_input')
         encoder_output,state_h,state_c=LSTM(self.hidden,return_state=True)(encoder_input)
         decoder_input=Input(shape=(None,self.max_vocab_len),name='decoder_input')
         decoder_output,_,_=LSTM(self.hidden,return_sequences=True,return_state=True)
 
 if __name__=='__main__':
-
-    d=Data(
-        train_data_path='/home/next/PycharmProjects/zhoujianyun/ConceptNet/train_2.tsv',
-        valid_data_path='/home/next/PycharmProjects/zhoujianyun/ConceptNet/valid_2.tsv',
-        batch_size=64,
-        concept_data_path='/home/next/PycharmProjects/zhoujianyun/ConceptNet/concept_dict.json'
-    )
-    d.generator().__next__()
+    computer=1
+    if computer==1:
+        d = Data(
+            train_data_path='/home/next/PycharmProjects/zhoujianyun/ConceptNet/train_2.tsv',
+            valid_data_path='/home/next/PycharmProjects/zhoujianyun/ConceptNet/valid_2.tsv',
+            batch_size=64,
+            concept_data_path='/home/next/PycharmProjects/zhoujianyun/ConceptNet/concept_dict.json'
+        )
+        d.generator().__next__()
+    else:
+        d = Data(
+            train_data_path='/home/next/PycharmProjects/zhoujianyun/ConceptNet/train_2.tsv',
+            valid_data_path='/home/next/PycharmProjects/zhoujianyun/ConceptNet/valid_2.tsv',
+            batch_size=64,
+            concept_data_path='/home/next/PycharmProjects/zhoujianyun/ConceptNet/concept_dict.json'
+        )
+        d.generator().__next__()
