6ca497c60e6e71d659e91dbf22c2bc343bb61315
李帅@NLP <tongjicslishuai@gmail.com>
2019-09-01 11:50:10 +0800

    add LSTM

--- keras_version/main.py
+++ keras_version/main.py
@@ -1,12 +1,12 @@
 import numpy as np
 from collections import defaultdict
-from keras.layers import Input,LSTM,GRU,Bidirectional,Lambda,Embedding,Permute,Reshape
-from keras.preprocessing.text import Tokenizer
+from keras.layers import Input,LSTM,Dense
 from keras import Model
+from keras.optimizers import Adam,SGD,RMSprop
 from keras.preprocessing.sequence import pad_sequences
 import keras.backend as K
-
 import json
+from keras.callbacks import EarlyStopping, ReduceLROnPlateau,TensorBoard,ModelCheckpoint
 class Data(object):
     def __init__(self,train_data_path='../Data/train_3.txt',
                  valid_data_path='../Data/valid_3.txt',
@@ -76,10 +76,6 @@ class Data(object):
 
                     utt_all.append(utt_id)
 
-                # for i in range(self.max_sen):
-                #     line_sample=[utt_all[j][i] for j in range(self.batch_size)]
-                #     line_sample=pad_sequences(line_sample,maxlen=max(utt_len))
-                #     encoder_input.append(line_sample)
                 for sample in utt_all:
                     sample=pad_sequences(sample,maxlen=max(utt_len))
                     encoder_input.append(sample)
@@ -95,56 +91,92 @@ class Data(object):
                 id = (id + self.batch_size) % (len(data))
                 encoder_input = []
                 decoder_input = []
-
         else:
             pass
 
 class seq2seq(object):
-    def __init__(self,hidden,max_vocab_len=600000,drop_out=0.1,max_sentence_len=250,max_sen=11,batch_size=64):
-        self.max_vocab_len=max_vocab_len
+    def __init__(self,hidden,max_sentence_len=250,max_sen=11,batch_size=8):
         self.max_sentence_len=max_sentence_len
-        self.drop_out=drop_out
         self.max_sen=max_sen
         self.hidden=hidden
         self.batch_size=batch_size
     def reshape(self,x):
         return K.reshape(x,shape=(self.batch_size,self.max_sen,self.hidden))
 
-    def build_network(self):
-        input_encoder=Input(name='encoder_input',batch_shape=(self.batch_size,self.max_sen,None))
-        embedding_layer=Embedding(self.max_vocab_len, self.hidden, input_length=None, batch_size=self.batch_size)
-        lstm_layer = LSTM(self.hidden, return_sequences=True,return_state=True,dropout=self.drop_out)
+    def build_network(self,max_vocab_len,baseline=True,is_training=True):
+        """
+        :param baseline:
+        :param is_training:
+        :return:
+        """
+        if baseline:
+            encoder_inputs = Input(shape=(None, max_vocab_len))
+            encoder = LSTM(self.hidden, return_state=True)
+            encoder_outputs, state_h, state_c = encoder(encoder_inputs)
+            encoder_states = [state_h, state_c]
 
-        input_shape=input_encoder.get_shape()
-        print('input shape:{}'.format(input_shape))
-        every_line_last_hidden_state=[]
-        every_line_hidden_state=[]
-        for line in range(int(input_shape[1])):
-            oneline=embedding_layer(input_encoder[:,line,:])
-            oneline,state_h, state_c=lstm_layer(oneline)
-            every_line_last_hidden_state.append(state_h)
-            every_line_hidden_state.append(K.concatenate(oneline,axis=-1))
-        x=K.concatenate(every_line_last_hidden_state,axis=-1)
-        print(x.get_shape())
+            decoder_inputs = Input(shape=(None, max_vocab_len))
+            decoder_lstm = LSTM(self.hidden, return_sequences=True, return_state=True)
+            decoder_outputs, _, _ = decoder_lstm(decoder_inputs,
+                                                 initial_state=encoder_states)
+            decoder_dense = Dense(max_vocab_len, activation='softmax')
+            decoder_outputs = decoder_dense(decoder_outputs)
+            model = Model([encoder_inputs, decoder_inputs], decoder_outputs) if is_training else Model(encoder_inputs, decoder_outputs)
+            model.summary()
+            return model
 
+    def train(self,baseline=True,train_data_path='../Data/train_3.txt',
+                 dict_path='../Data/all_dict.json'):
+        train_data=open(train_data_path,'r',encoding='utf-8').readlines()
+        dict_data=json.load(open(dict_path,'r',encoding='utf-8'))
+        replace_key = list(dict_data.keys())[0]
+        dict_data['<pad>'] = 0
+        dict_data['<eou>'] = len(dict_data.keys())
+        dict_data[replace_key] = len(dict_data.keys())
+        num_tokens=len(dict_data.keys())
+        input_texts=[]
+        input_len=[]
+        target_texts=[]
+        target_len=[]
+        for sample in train_data:
+            temp=sample.split('\t')
+            input=temp[0].split(' ')
+            input_len.append(len(input))
+            input_texts.append(input)
+            target=sample[1].split(' ')
+            target_len.append(len(target))
+            target_texts.append(target)
 
-
-        # x=K.concatenate(x,axis=-1)
-        # x=Lambda(function=self.reshape)(x)
-        # print(x.get_shape())
-        # dialog=LSTM(self.hidden,dropout=self.drop_out)(x)
-        # print('dialog shape:',dialog.get_shape())
-        #
-        # response_input=Input(name='decoder_input',batch_shape=(self.batch_size,None))
-
-
-
-
-        # print(x.get_shape())
-
+        encoder_input_data = np.zeros(
+            (len(train_data), max(input_len), num_tokens),
+            dtype='int32')
+        decoder_input_data = np.zeros(
+            (len(train_data), max(target_len), num_tokens),
+            dtype='int32')
+        decoder_target_data = np.zeros(
+            (len(train_data),max(target_len), num_tokens),
+            dtype='int32')
+        for i,(input_text,target_text) in enumerate(zip(input_texts,target_texts)):
+            for t, word in enumerate(input_text):
+                encoder_input_data[i,t,dict_data[word]]=1.
+            for t,word in enumerate(target_text):
+                decoder_input_data[i,t,dict_data[word]]=1.
+                if t>0:
+                    decoder_target_data[i, t - 1, dict_data[word]] = 1.
+        model=self.build_network(max_vocab_len=len(dict_data.keys()),is_training=True,baseline=baseline)
+        model.compile(
+            optimizer=Adam(lr=0.01),
+            loss=['categorical_crossentropy']
+        )
+        model.fit(x=[encoder_input_data, decoder_input_data],y=decoder_target_data,
+                  batch_size=self.batch_size,
+                  epochs=100,initial_epoch=0,validation_split=0.2,
+                  callbacks=[
+                      TensorBoard('logs')
+                  ])
 
 if __name__=='__main__':
-    computer=1
+    computer=2
     if computer==1:
         d = Data()
         t=d.generator()
@@ -153,13 +185,9 @@ if __name__=='__main__':
         model=seq2seq(hidden=128)
         model.build_network()
     else:
-        d = Data(
-            train_data_path='/home/next/PycharmProjects/zhoujianyun/ConceptNet/train_2.tsv',
-            valid_data_path='/home/next/PycharmProjects/zhoujianyun/ConceptNet/valid_2.tsv',
-            batch_size=64,
-            concept_data_path='/home/next/PycharmProjects/zhoujianyun/ConceptNet/concept_dict.json'
-        )
-        t=d.generator()
+        model = seq2seq(hidden=128)
+        model.train(baseline=True,train_data_path='F:\Dialogue_System\zhoujianyun\ConceptNet\\train_3.txt',
+            dict_path='F:\Dialogue_System\zhoujianyun\ConceptNet\\all_dict.json')
 
 
 
--- keras_version/test.py
+++ keras_version/test.py
@@ -1,39 +1,126 @@
-# for i in train_data:
-        #     temp=i.split('\t')
-        #     word_len=[len(s.split(' ')) for s in temp[0].split('<eou>')]
-        #     word_len.append(len(temp[1].split(' ')))
-        #     if len(temp)==3 and len(temp[0].split('<eou>'))<=self.max_sen and max(word_len)<=self.max_len:
-        #         train_data_.append(i)
-        #         words=temp[0].split(' ')+temp[1].split(' ')
-        #         for j in words:
-        #             self.dict.add(j)
-        #             try:
-        #                 expand_list=self.concept_data[j].keys()
-        #                 for t in expand_list:
-        #                     self.dict.add(t)
-        #             except:
-        #                 pass
-        # for i in valid_data:
-        #     temp=i.split('\t')
-        #     word_len = [len(s.split(' ')) for s in temp[0].split('<eou>')]
-        #     word_len.append(len(temp[1].split(' ')))
-        #     if len(temp)==3 and len(temp[0].split('<eou>'))<=self.max_sen and max(word_len)<=self.max_len:
-        #         valid_data_.append(i)
-        #         words=temp[0].split(' ')+temp[1].split(' ')
-        #         for j in words:
-        #             self.dict.add(j)
-        #             try:
-        #                 expand_list = self.concept_data[j].keys()
-        #                 for t in expand_list:
-        #                     self.dict.add(t)
-        #             except:
-        #                 pass
-        #
-        # print(len(train_data),len(train_data_))
-        # # with open('../Data/train_3.txt','w',encoding='utf-8') as f:
-        # #     f.writelines(train_data_)
-        # # with open('../Data/valid_3.txt','w',encoding='utf-8') as f:
-        # #     f.writelines(valid_data_)
-        # dict_index={w:i for i,w in enumerate(self.dict)}
-        # with open('../Data/all_dict.json','w',encoding='utf-8') as f:
-        #     json.dump(dict_index,f)
+from __future__ import print_function
+
+from keras.models import Model
+from keras.layers import Input, LSTM, Dense
+import numpy as np
+
+batch_size = 64  # Batch size for training.
+epochs = 100  # Number of epochs to train for.
+latent_dim = 256  # Latent dimensionality of the encoding space.
+num_samples = 10000  # Number of samples to train on.
+# Path to the data txt file on disk.
+data_path = '../fra-eng/fra.txt'
+
+# Vectorize the data.
+input_texts = []
+target_texts = []
+input_characters = set()
+target_characters = set()
+with open(data_path, 'r', encoding='utf-8') as f:
+    lines = f.read().split('\n')
+for line in lines[: min(num_samples, len(lines) - 1)]:
+    input_text, target_text = line.split('\t')
+    # We use "tab" as the "start sequence" character
+    # for the targets, and "\n" as "end sequence" character.
+    target_text = '\t' + target_text + '\n'
+    input_texts.append(input_text)
+    target_texts.append(target_text)
+    for char in input_text:
+        if char not in input_characters:
+            input_characters.add(char)
+    for char in target_text:
+        if char not in target_characters:
+            target_characters.add(char)
+
+input_characters = sorted(list(input_characters))
+target_characters = sorted(list(target_characters))
+num_encoder_tokens = len(input_characters)
+num_decoder_tokens = len(target_characters)
+max_encoder_seq_length = max([len(txt) for txt in input_texts])
+max_decoder_seq_length = max([len(txt) for txt in target_texts])
+
+print('Number of samples:', len(input_texts))
+print('Number of unique input tokens:', num_encoder_tokens)
+print('Number of unique output tokens:', num_decoder_tokens)
+print('Max sequence length for inputs:', max_encoder_seq_length)
+print('Max sequence length for outputs:', max_decoder_seq_length)
+
+input_token_index = dict(
+    [(char, i) for i, char in enumerate(input_characters)])
+target_token_index = dict(
+    [(char, i) for i, char in enumerate(target_characters)])
+
+encoder_input_data = np.zeros(
+    (len(input_texts), max_encoder_seq_length, num_encoder_tokens),
+    dtype='float32')
+decoder_input_data = np.zeros(
+    (len(input_texts), max_decoder_seq_length, num_decoder_tokens),
+    dtype='float32')
+decoder_target_data = np.zeros(
+    (len(input_texts), max_decoder_seq_length, num_decoder_tokens),
+    dtype='float32')
+
+for i, (input_text, target_text) in enumerate(zip(input_texts, target_texts)):
+    for t, char in enumerate(input_text):
+        encoder_input_data[i, t, input_token_index[char]] = 1.
+    for t, char in enumerate(target_text):
+        # decoder_target_data is ahead of decoder_input_data by one timestep
+        decoder_input_data[i, t, target_token_index[char]] = 1.
+        if t > 0:
+            # decoder_target_data will be ahead by one timestep
+            # and will not include the start character.
+            decoder_target_data[i, t - 1, target_token_index[char]] = 1.
+
+# Define an input sequence and process it.
+encoder_inputs = Input(shape=(None, num_encoder_tokens))
+encoder = LSTM(latent_dim, return_state=True)
+encoder_outputs, state_h, state_c = encoder(encoder_inputs)
+# We discard `encoder_outputs` and only keep the states.
+encoder_states = [state_h, state_c]
+
+# Set up the decoder, using `encoder_states` as initial state.
+decoder_inputs = Input(shape=(None, num_decoder_tokens))
+# We set up our decoder to return full output sequences,
+# and to return internal states as well. We don't use the
+# return states in the training model, but we will use them in inference.
+decoder_lstm = LSTM(latent_dim, return_sequences=True, return_state=True)
+decoder_outputs, _, _ = decoder_lstm(decoder_inputs,
+                                     initial_state=encoder_states)
+decoder_dense = Dense(num_decoder_tokens, activation='softmax')
+decoder_outputs = decoder_dense(decoder_outputs)
+
+# Define the model that will turn
+# `encoder_input_data` & `decoder_input_data` into `decoder_target_data`
+model = Model([encoder_inputs, decoder_inputs], decoder_outputs)
+model.summary()
+# encoder_lstm_layer = LSTM(self.hidden, return_state=True)
+            # input_shape = input_encoder.get_shape()
+            # print('input shape:{}'.format(input_shape))
+            # encoder_embedding_tensor = embedding_layer(input_encoder)
+            # encoder_embedding_tensor_shape = encoder_embedding_tensor.get_shape()
+            # print('embedding_encoder_tensor_shape is {}'.format(encoder_embedding_tensor_shape))
+            # hidden_h, hidden_c = None, None
+            # # 这里baseline没有使用到hidden_h和hidden_c 形成新的序列来做 lstm形成dialog_hidden
+            # for line in range(self.max_sen):
+            #     single_line_tensor = encoder_embedding_tensor[:, line, :, :]
+            #     if line == 0:
+            #         encoder_outputs, hidden_h, hidden_c = encoder_lstm_layer(single_line_tensor)
+            #     else:
+            #         encoder_outputs, hidden_h, hidden_c = encoder_lstm_layer(single_line_tensor,
+            #                                                                  initial_state=[hidden_h, hidden_c])
+            #     print(encoder_outputs.get_shape(), hidden_h.get_shape(), hidden_c.get_shape())
+            #
+            # decoder_input = Input(shape=(None,), name='decoder_input')
+            # decoder_embedding_tensor = embedding_layer(decoder_input)
+            # decoder_lstm_layer = LSTM(self.hidden, return_sequences=True)
+            # print('*' * 100)
+            # print(decoder_embedding_tensor.get_shape())
+            # decoder_outputs = decoder_lstm_layer(decoder_embedding_tensor, initial_state=[hidden_h, hidden_c])
+            # print(decoder_outputs.get_shape())
+            # decoder_predict = Dense(self.max_vocab_len, activation='softmax')(decoder_outputs)
+            # print(decoder_predict.get_shape())
+            # model = Model([input_encoder, decoder_input], decoder_outputs) if is_training else Model(input_encoder,
+            #                                                                                          decoder_predict)
+            # model.summary()
+            # return model
+
