7ceef978b1553bffa80908acaccc134b854a6533
TomHacker <3472661791@qq.com>
2019-09-03 13:16:22 +0800

    upgrade the speed of generator

--- keras_version/main.py
+++ keras_version/main.py
@@ -208,5 +208,5 @@ class seq2seq(object):
 if __name__=='__main__':
     #'/diskA/wenqiang/lishuai/seq2seq_stable/Data/train_3.txt'
     app=seq2seq(hidden=256)
-    app.train(batch_size=32,baseline=True,union=True,hierarchical=False,split_ratio=0.2,
+    app.train(batch_size=32,baseline=True,union=False,hierarchical=False,split_ratio=0.2,
               train_data_path='../Data/train_3.txt')
